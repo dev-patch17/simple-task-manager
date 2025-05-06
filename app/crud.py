f79a08bc35ec886fc301e8424dd62cbef17cb268
Benjamin Patch <ben@benjaminpatch.com>
2025-05-06 15:16:53 -0700

    Write 'task' CRUD

--- app/crud.py
+++ app/crud.py
@@ -3,6 +3,7 @@ from . import models, schemas
 
 # project CRUD
 
+# prevent more than 100 projects to be called from the db at once
 def get_projects(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Project).offset(skip).limit(limit).all()
 
@@ -32,6 +33,41 @@ def delete_project(db: Session, project_id: int):
     db_project = get_project(db, project_id)
     if db_project:
         db.delete(db_project)
+        db.commit()
+        return True
+    return False
+
+
+# task CRUD
+
+# prevent more than 100 tasks to be called from the db at once
+def get_tasks(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Task).offset(skip).limit(limit).all()
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    db_task = models.Task(**task.dict())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def update_task(db: Session, task_id: int, task: schemas.TaskCreate):
+    db_task = get_task(db, task_id)
+    if db_task:
+        update_data = task.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(db_task, key, value)
+        db.commit()
+        db.refresh(db_task)
+    return db_task
+
+def delete_task(db: Session, task_id: int):
+    db_task = get_task(db, task_id)
+    if db_task:
+        db.delete(db_task)
         db.commit()
         return True
     return False
