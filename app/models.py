52bf65265a1d9652f0039984108366c6d8a81953
Benjamin Patch <ben@benjaminpatch.com>
2025-05-05 22:40:23 -0700

    Define 'Project' table

--- app/models.py
+++ app/models.py
@@ -1,1 +1,19 @@
-# SQLAlchemy models
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from .database import Base
+
+# define the database models
+
+class Project(Base):
+  __tablename__ = 'projects'
+
+  id = Column(Integer, primary_key=True, index=True)
+  title = Column(String, index=True)
+  description = Column(String)
+  completed = Column(Boolean, default=False)
+  created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+  # relationship with tasks
+  tasks = relationship('Task', back_populates='project', cascade='all, delete-orphan')
