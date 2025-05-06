c4d061d11b81986bc206afd28eb6a21ea16db26a
Benjamin Patch <ben@benjaminpatch.com>
2025-05-06 15:30:07 -0700

    Create db tables and initialize FastAPI

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# FastAPI application
+from fastapi import FastAPI, Depends, Request, Form, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, RedirectResponse
+from sqlalchemy.orm import Session
+from typing import Optional
+
+from . import models, schemas, crud
+from .database import engine, get_db
+
+# create database tables
+models.Base.metadata.create_all(bind=engine)
+
+# initialize FastAPI
+app = FastAPI(title='Simple Task Manager')
