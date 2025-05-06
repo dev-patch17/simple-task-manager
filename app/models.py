0c8374e2abb6ab9e1015029a69d6bd268f1bbd9f
Benjamin Patch <ben@benjaminpatch.com>
2025-05-05 23:04:58 -0700

    Define 'Task' table

--- app/models.py
+++ app/models.py
@@ -11,9 +11,22 @@ class Project(Base):
 
   id = Column(Integer, primary_key=True, index=True)
   title = Column(String, index=True)
-  description = Column(String)
+  notes = Column(String)
   completed = Column(Boolean, default=False)
   created_at = Column(DateTime(timezone=True), server_default=func.now())
 
   # relationship with tasks
   tasks = relationship('Task', back_populates='project', cascade='all, delete-orphan')
+
+class Task(Base):
+  __tablename__ = 'tasks'
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String, index=True)
+  notes = Column(String)
+  completed = Column(Boolean, default=False)
+  created_at = Column(DateTime(timezone=True), server_default=func.now())
+  project_id = Column(Integer, ForeignKey('projects.id'), nullable=True)
+
+  # relationship with project
+  project = relationship('Project', back_populates='tasks')
