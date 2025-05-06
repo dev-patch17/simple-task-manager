60c5dfc8ddcb60b19c1dbd8c6ab54886a8924c36
Benjamin Patch <ben@benjaminpatch.com>
2025-05-06 12:10:40 -0700

    Complete task validation schemas

--- app/schemas.py
+++ app/schemas.py
@@ -9,3 +9,19 @@ class TaskBase(BaseModel):
     notes: Optional[str] = None
     completed: bool = False
     project_id: Optional[int] = None
+
+# if we later decide new tasks require additional fields during creation,
+# please add them here
+class TaskCreate(TaskBase):
+    pass
+
+# represents the complete task as it exists in the database
+# including fields that cannot be set by the user directly
+class Task(TaskBase):
+    id: int
+    created_at: datetime
+
+    # allow Pydantic to convert ORM objects to dictionaries
+    # and access nested relationships
+    class Config:
+        orm_mode = True
