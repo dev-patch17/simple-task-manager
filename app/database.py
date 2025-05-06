5e810fb024f4e66a60f9419e38e9039e6dd66c05
Benjamin Patch <ben@benjaminpatch.com>
2025-05-05 18:27:39 -0700

    Start building the database connection

--- app/database.py
+++ app/database.py
@@ -1,1 +1,16 @@
 # database connection
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./simple_task_manager.db"
+
+engine = create_engine(
+  SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# continue with dependency to get DB session
