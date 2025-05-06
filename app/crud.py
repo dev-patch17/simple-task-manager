e64ad16243d616a3afe6e5331755cbb3e6a8d6e1
Benjamin Patch <ben@benjaminpatch.com>
2025-05-06 12:42:01 -0700

    Write 'project' CRUD

--- app/crud.py
+++ app/crud.py
@@ -1,1 +1,37 @@
-# CRUD operations
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# project CRUD
+
+def get_projects(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Project).offset(skip).limit(limit).all()
+
+def get_project(db: Session, project_id: int):
+    return db.query(models.Project).filter(
+        models.Project.id == project_id).first()
+
+def create_project(db: Session, project: schemas.ProjectCreate):
+    db_project = models.Project(**project.dict())
+    db.add(db_project)
+    db.commit()
+    db.refresh(db_project)
+    return db_project
+
+def update_project(
+        db: Session, project_id: int, project: schemas.ProjectCreate):
+    db_project = get_project(db, project_id)
+    if db_project:
+        update_data = project.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(db_project, key, value)
+        db.commit()
+        db.refresh(db_project)
+    return db_project
+
+def delete_project(db: Session, project_id: int):
+    db_project = get_project(db, project_id)
+    if db_project:
+        db.delete(db_project)
+        db.commit()
+        return True
+    return False
