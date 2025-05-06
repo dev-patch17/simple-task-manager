19457136d77a96b522531cc8bba972ff42990d07
Benjamin Patch <ben@benjaminpatch.com>
2025-05-05 23:21:05 -0700

    Document db connection with more comments

--- app/database.py
+++ app/database.py
@@ -1,16 +1,18 @@
-# database connection
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./simple_task_manager.db"
 
+# create an SQLAlchemy engine,
+# which is the starting point for any SQLAlchemy application
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+# create a factory for creating database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# create a base class that the model classes will inherit from
 Base = declarative_base()
 
 # dependency for routes to open and close a DB session
